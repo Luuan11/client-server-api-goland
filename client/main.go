94452a27f3aa8fec52530bac012f03bc5350e230
Luuan11 <luancraft55@gmail.com>
2025-03-22 11:55:10 -0300

    :sparkles: feat: Doing request

--- client/main.go
+++ client/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
 func main() {
-	
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
 }
