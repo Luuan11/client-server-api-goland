0ca75b99154176ebb199d472a2a3a3962694b365
Luuan11 <luancraft55@gmail.com>
2025-03-23 23:30:16 -0300

    :sparkles: feat: finishing client side

--- client/main.go
+++ client/main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"time"
+    "context"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "time"
 )
 
+type ExchangeRateResponse struct {
+    Dolar string `json:"Dólar"`
+}
+
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+    ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+    defer cancel()
 
-	defer cancel()
+    req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        return
+    }
 
-	req, err := http.NewRequestWithContext(ctx, "GET", "", nil)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        fmt.Println("Error making request:", err)
+        return
+    }
+    defer resp.Body.Close()
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+    if resp.StatusCode != http.StatusOK {
+        body, _ := io.ReadAll(resp.Body)
+        fmt.Println("Error response from server:", string(body))
+        return
+    }
 
-	defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Error reading response body:", err)
+        return
+    }
 
+    var exchangeRate ExchangeRateResponse
+    if err := json.Unmarshal(body, &exchangeRate); err != nil {
+        fmt.Println("Error unmarshaling response:", err)
+        return
+    }
+
+    fileContent := fmt.Sprintf("Dólar: %s", exchangeRate.Dolar)
+    if err := os.WriteFile("cotacao.txt", []byte(fileContent), os.ModePerm); err != nil {
+        fmt.Println("Error writing to file:", err)
+        return
+    }
+
+    fmt.Println("Saved exchange rate to cotacao.txt")
 }
